c2c373693f8fc323bb2a5d09d87d8be0eb03db45
Gui Barscevicius <guilbrs@gmail.com>
2019-01-02 18:17:17 -0200

    add aliases, fix docker connection

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,33 @@
 const path = require('path')
-const alias = p => path.resolve(__dirname, p)
+const alias = p => path.resolve(__dirname, 'src/' + p)
 
 module.exports = {
   chainWebpack: config => {
     config.resolve.alias
-      .set('@assets', alias('src/assets'))
-      .set('@components', alias('src/components'))
-      .set('@containers', alias('src/containers'))
-      .set('@modules', alias('src/modules'))
-      .set('@router', alias('src/router'))
-      .set('@store', alias('src/store'))
-      .set('@styles', alias('src/styles'))
-      .set('@types', alias('src/store/types'))
-      .set('@templates', alias('src/templates'))
+      .set('@assets', alias('assets'))
+      .set('@components', alias('components'))
+      .set('@containers', alias('containers'))
+      .set('@helpers', alias('helpers'))
+      .set('@modules', alias('modules'))
+      .set('@router', alias('router'))
+      .set('@store', alias('store'))
+      .set('@styles', alias('styles'))
+      .set('@templates', alias('templates'))
+      .set('@types', alias('store/types'))
 
     config.resolve.extensions
       .add('.scss')
+      .add('.svg')
   },
 
   devServer: {
     host: '0.0.0.0',
     hot: true,
     disableHostCheck: true,
+    watchOptions: {
+      ignored: /node_modules/,
+      aggregteTimeout: 300,
+      poll: 1000
+    },
   },
 }
