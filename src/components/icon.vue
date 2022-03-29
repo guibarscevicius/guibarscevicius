1d4730edbf90e64d5a3ec2adcb10e99fdb5ef336
Gui Barscevicius <guilbrs@gmail.com>
2022-03-29 16:09:25 -0300

    icon components

---
+++ src/components/icon.vue
@@ -0,0 +1,15 @@
+<script>
+import { h } from 'vue'
+
+export default {
+  props: {
+    href: String,
+    class: String,
+  },
+  setup(props) {
+    return () => h('a', { href: props.href },
+      h('div', { class: `i-${props.class}` })
+    )
+  }
+}
+</script>
