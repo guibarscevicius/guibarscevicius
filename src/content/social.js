e1a19be59e65f757f8513c9aa0ef29dd77348acd
Gui Barscevicius <guilbrs@gmail.com>
2022-03-29 16:09:47 -0300

    enhance social content

---
+++ src/content/social.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    class: 'logos-codepen-icon',
+    href: 'https://codepen.io/gui',
+  },
+  {
+    class: 'logos-vue',
+    href: 'mailto:contato@guibarscevicius.com.br',
+  },
+  // {
+  //   name: 'github',
+  //   href: 'https://github.com/guibarscevicius',
+  // },
+  // {
+  //   name: 'linkedin',
+  //   href: 'https://www.linkedin.com/in/guibarscevicius',
+  // },
+  // // {
+  // //   name: 'twitter',
+  // //   href: 'https://twitter.com/gbarscevicius',
+  // // },
+  // {
+  //   name: 'whatsapp',
+  //   href: 'https://wa.me/+5511995180205',
+  // }
+]
