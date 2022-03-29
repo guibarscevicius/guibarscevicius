800e5a215ba5969c6c75676de772e1755aaf3e76
Gui Barscevicius <guilbrs@gmail.com>
2022-03-29 16:37:48 -0300

    icons

--- src/content/social.js
+++ src/content/social.js
@@ -1,26 +1,14 @@
 export default [
   {
-    class: 'logos-codepen-icon',
-    href: 'https://codepen.io/gui',
-  },
-  {
-    class: 'logos-vue',
+    class: 'mdi-email-outline',
     href: 'mailto:contato@guibarscevicius.com.br',
   },
-  // {
-  //   name: 'github',
-  //   href: 'https://github.com/guibarscevicius',
-  // },
-  // {
-  //   name: 'linkedin',
-  //   href: 'https://www.linkedin.com/in/guibarscevicius',
-  // },
-  // // {
-  // //   name: 'twitter',
-  // //   href: 'https://twitter.com/gbarscevicius',
-  // // },
-  // {
-  //   name: 'whatsapp',
-  //   href: 'https://wa.me/+5511995180205',
-  // }
+  {
+    class: 'mdi-github',
+    href: 'https://github.com/guibarscevicius',
+  },
+  {
+    class: 'mdi-linkedin',
+    href: 'https://www.linkedin.com/in/guibarscevicius',
+  },
 ]
