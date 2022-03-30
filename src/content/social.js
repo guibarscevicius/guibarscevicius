8671f9ad10cdc2871163dde6359cb8e4d3f60257
Gui Barscevicius <guilbrs@gmail.com>
2022-03-30 13:18:48 -0300

    add social stuff names

--- src/content/social.js
+++ src/content/social.js
@@ -1,13 +1,16 @@
 export default [
   {
+    name: 'Linkedin',
     class: 'mdi-linkedin',
     href: 'https://www.linkedin.com/in/guibarscevicius',
   },
   {
+    name: 'Github',
     class: 'mdi-github',
     href: 'https://github.com/guibarscevicius',
   },
   {
+    name: 'Email',
     class: 'mdi-email-outline',
     href: 'mailto:contato@guibarscevicius.com.br',
   },
